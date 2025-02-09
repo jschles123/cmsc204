7bf66f69ba8fd125357d3ae8adda35380c7291b5
jschles123 <jschles1@montgomerycollege.edu>
2025-02-09 01:30:05 -0500

    done

--- Assignment1/PasswordCheckerTest_STUDENT.java
+++ Assignment1/PasswordCheckerTest_STUDENT.java
@@ -5,21 +5,29 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 /**
  * STUDENT tests for the methods of PasswordChecker
  * @author Joshua Schlesinger-Guevara
  *
  */
 public class PasswordCheckerTest_STUDENT {
+	ArrayList<String> passwords;
+
 
 	@Before
 	public void setUp() throws Exception {
-		
+		passwords = new ArrayList<String>();
+		String[] pass = {"ValidPas1!","TwoCharss1!","Sht1!","Weak1!","nowupperpass12!","NOLOWERPASS12!","NoNumbers!","NoSpecialChars1"};
+		for(String i : pass){
+			passwords.add(i);
+		}
 	}
 
 	@After
 	public void tearDown() throws Exception {
-
+		passwords = null;
 	}
 
 	/**
@@ -29,7 +37,19 @@ public class PasswordCheckerTest_STUDENT {
 	@Test
 	public void testIsValidPasswordTooShort()
 	{
-		fail("Not implemented by student yet");
+		try {
+			PasswordCheckerUtility.isValidPassword(passwords.get(0));
+		} catch (Exception e) {
+			fail();
+		}
+		try {
+			PasswordCheckerUtility.isValidPassword(passwords.get(2));
+			fail("Expected LengthException");
+		} catch (LengthException e) {
+			// Expected exception
+		} catch (Exception e) {
+			fail();
+		}
 	}
 	
 	/**
@@ -39,7 +59,19 @@ public class PasswordCheckerTest_STUDENT {
 	@Test
 	public void testIsValidPasswordNoUpperAlpha()
 	{
-		fail("Not implemented by student yet");
+		try {
+			PasswordCheckerUtility.isValidPassword(passwords.get(0));
+		} catch (Exception e) {
+			fail();
+		}
+		try {
+			PasswordCheckerUtility.isValidPassword(passwords.get(4));
+			fail("Expected NoUpperAlphaException");
+		} catch (NoUpperAlphaException e) {
+			// Expected exception
+		} catch (Exception e) {
+			fail();
+		}
 	}
 	
 	/**
@@ -49,7 +81,19 @@ public class PasswordCheckerTest_STUDENT {
 	@Test
 	public void testIsValidPasswordNoLowerAlpha()
 	{
-		fail("Not implemented by student yet");
+		try {
+			PasswordCheckerUtility.isValidPassword(passwords.get(0));
+		} catch (Exception e) {
+			fail();
+		}
+		try {
+			PasswordCheckerUtility.isValidPassword(passwords.get(5));
+			fail("Expected NoLowerAlphaException");
+		} catch (NoLowerAlphaException e) {
+			// Expected exception
+		} catch (Exception e) {
+			fail();
+		}
 	}
 	/**
 	 * Test if the password has more than 2 of the same character in sequence
@@ -58,7 +102,19 @@ public class PasswordCheckerTest_STUDENT {
 	@Test
 	public void testIsWeakPassword()
 	{
-		fail("Not implemented by student yet");
+		try {
+			PasswordCheckerUtility.isWeakPassword(passwords.get(0));
+		} catch (Exception e) {
+			fail();
+		}
+		try {
+			PasswordCheckerUtility.isWeakPassword(passwords.get(3));
+			fail("Expected WeakPasswordException");
+		} catch (WeakPasswordException e) {
+			// Expected exception
+		} catch (Exception e) {
+			fail();
+		}
 	}
 	
 	/**
@@ -68,7 +124,19 @@ public class PasswordCheckerTest_STUDENT {
 	@Test
 	public void testIsValidPasswordInvalidSequence()
 	{
-		fail("Not implemented by student yet");
+		try {
+			PasswordCheckerUtility.isValidPassword(passwords.get(0));
+		} catch (Exception e) {
+			fail();
+		}
+		try {
+			PasswordCheckerUtility.isValidPassword(passwords.get(1));
+			fail("Expected InvalidSequenceException");
+		} catch (InvalidSequenceException e) {
+			// Expected exception
+		} catch (Exception e) {
+			fail();
+		}
 	}
 	
 	/**
@@ -78,7 +146,19 @@ public class PasswordCheckerTest_STUDENT {
 	@Test
 	public void testIsValidPasswordNoDigit()
 	{
-		fail("Not implemented by student yet");
+		try {
+			PasswordCheckerUtility.isValidPassword(passwords.get(0));
+		} catch (Exception e) {
+			fail();
+		}
+		try {
+			PasswordCheckerUtility.isValidPassword(passwords.get(6));
+			fail("Expected NoDigitException");
+		} catch (NoDigitException e) {
+			// Expected exception
+		} catch (Exception e) {
+			fail();
+		}
 	}
 	
 	/**
@@ -88,7 +168,17 @@ public class PasswordCheckerTest_STUDENT {
 	@Test
 	public void testIsValidPasswordSuccessful()
 	{
-		fail("Not implemented by student yet");
+		try {
+			PasswordCheckerUtility.isValidPassword(passwords.get(0));
+		} catch (Exception e) {
+			fail();
+		}
+		try {
+			PasswordCheckerUtility.isValidPassword(passwords.get(6));
+			fail("Expected NoDigitException");
+		} catch (Exception e) {
+			//Expected
+		}
 	}
 	
 	/**
@@ -97,7 +187,18 @@ public class PasswordCheckerTest_STUDENT {
 	 */
 	@Test
 	public void testInvalidPasswords() {
-		fail("Not implemented by student yet");
+		ArrayList<String> Actual = PasswordCheckerUtility.getInvalidPasswords(passwords);
+		String[] temp = {"TwoCharss1! The password cannot contain more than two of the same character in sequence",
+				"Sht1! The password must be at least 6 characters long",
+				"nowupperpass12! The password must contain at least one uppercase alphabetic character"
+				,"NOLOWERPASS12! The password must contain at least one lowercase alphabetic character",
+				"NoNumbers! The password must contain at least one digit"
+				,"NoSpecialChars1 The password must contain at least one special character"};
+		ArrayList<String> Expected = new ArrayList<>();
+		for (String i : temp){
+			Expected.add(i);
+		}
+		assertTrue(Actual.equals(Expected));
 	}
 	
 }
