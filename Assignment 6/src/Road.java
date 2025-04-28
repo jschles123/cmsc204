7801cb208670f697c01a4f2bd5777ea60b39fca5
jschles123 <jschles1@montgomerycollege.edu>
2025-04-27 20:28:55 -0400

    Add files via upload

---
+++ Assignment 6/src/Road.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Road {
+
+
+
+
+    public Road(){
+
+
+    }
+
+
+
+
+}
---
+++ Assignment 6/src/Town.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class Town implements Comparable<Town>{
+
+
+    private String name;
+    private ArrayList<Town> towns;
+
+    Town(String name) {
+        this.name = name;
+        this.towns = new ArrayList<>();
+    }
+
+
+
+
+
+    public String getName(){
+        return this.name;
+    }
+
+    @Override
+    public int compareTo(Town other) {
+        return this.name.equals(other.getName()) ? 0 : -1;
+    }
+}
